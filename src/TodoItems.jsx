cb6a3c8e8c9e5c3771417da2252d72ddccb29d05
blake <zuttton@gmail.com>
2018-01-31 13:52:40 -0500

    Fixed an issue with TodoItems. App has to be js.

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -4,16 +4,16 @@ import FlipMove from 'react-flip-move';
 class TodoItems extends Component {
   constructor(props, context) {
     super(props, context);
-    this.removeList = this.removeList.bind(this);
+    this.removeItem = this.removeItem.bind(this);
     this.createTasks = this.createTasks.bind(this);
   }
 
-  removeList(key) {
-    this.props.removeList(key);
+  removeItem(key) {
+    this.props.removeItem(key);
   }
 
   createTasks(item) {
-    return <li onClick={() => this.removeList(item.key)} key={item.key}>{item.text}</li>
+    return <li onClick={() => this.removeItem(item.key)} key={item.key}>{item.text}</li>
   }
 
   render() {
